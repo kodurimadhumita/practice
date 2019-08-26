4c8d60f2e6f776a0f38215226270a08e3a56c0ec
kodurimadhumita <kodurimadhumita@gmail.com>
2019-08-26 09:56:34 +0530

    Pattern4

--- java/Pattern4.java
+++ java/Pattern4.java
@@ -1,7 +1,25 @@
-class Pattern4
+
+   /*         1
+            2 1
+		  3 2 1
+		4 3 2 1
+		*/
+
+
+
+
+
+
+class Stars2
 {
 	public static void main(String args[])
 	{
-		System.out.println("Pattern4 ");
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=4;j++)
+			{
+				if(i>=j)
+				{
+		System.out.println(*);
 	}
 }

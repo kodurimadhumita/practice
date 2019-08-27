09d74872d29ba186c41b0e344118b98e5005e482
kodurimadhumita <kodurimadhumita@gmail.com>
2019-08-27 20:21:32 +0530

    Stars4

--- java/Stars4.java
+++ java/Stars4.java
@@ -2,9 +2,9 @@ class Stars4
 {
 	public static void main(String args[])
 	{
-		for(int i=1;i<=5;i++)
+		for(int i=1;i<=4;i++)
 		{
-			for(int j=1;j<=5;j++)
+			for(int j=1;j<=4;j++)
 			{
 				if(i>=j)
 				{

29176f9e7674630dc6a3cd8c11f33b4c75f53986
kodurimadhumita <kodurimadhumita@gmail.com>
2019-09-02 17:49:46 +0530

    Stars2

--- java/Stars2.java
+++ java/Stars2.java
@@ -1,10 +1,10 @@
 /*
 print the given program
-           *
-         * *
-       * * *
-     * * * *
-   * * * * *
+         * 
+      * *
+    * * *
+  * * * *
+* * * * *
 
 */
 
@@ -15,12 +15,18 @@ class Stars2
 {
 	public static void main(String args[])
 	{
-		for(int i=1;i<=4;i++)
+		for(int i=5;i>=1;i--)
 		{
-			for(int j=1;j<=4;j++)
+			for(int j=1;j<=5;j++)
 			{
-				if(i>=j)
-				{
-		System.out.println(*);
-	}
-}
+                                                 if(i<=j)
+				
+		{	
+		System.out.print("*");
+	                    }
+                                                else
+                                        {
+                                    System.out.print(" ");
+}}
+                                System.out.println();
+}}}

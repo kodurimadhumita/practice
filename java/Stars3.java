703d2259d7db4b503a1f1b26a32a8cbd1d557b8f
kodurimadhumita <kodurimadhumita@gmail.com>
2019-09-02 18:08:50 +0530

    Stars3

--- java/Stars3.java
+++ java/Stars3.java
@@ -1,13 +1,30 @@
-class Stars4
+/*
+print the given stars
+* * * * *
+  * * * *
+    * * *
+      * *
+        *
+*/
+
+       
+class Stars3
 {
 	public static void main(String args[])
 	{
-		for(int i=1;i<=5;i++)
+		for(int i=5;i>=1;i--)
 		{
-			for(int j=1;j<=5;j++)
+			for(int j=5;j>=1;j--)
 			{
-				if(i>=j)
-				{
-		System.out.println(*);
+			if(i>=j)
+                                            {	
+				
+		System.out.print("*");
 	}
+                           else
+                                   {
+                                    System.out.print(" ");
+}}
+                                   System.out.println();
+}}
 }

ffd04aa7e48b7383819571961dcbf84a7ef3b4ca
kodurimadhumita <kodurimadhumita@gmail.com>
2019-09-03 15:16:49 +0530

    Stars4

--- java/Stars4.java
+++ java/Stars4.java
@@ -1,13 +1,32 @@
+/*
+print the given Stars
+* * * * *
+* * * *
+* * *
+* *
+*
+  */
+
+
 class Stars4
 {
 	public static void main(String args[])
 	{
-		for(int i=1;i<=4;i++)
+		for(int i=5;i>=1;i--)
 		{
-			for(int j=1;j<=4;j++)
+			for(int j=1;j<=5;j++)
 			{
 				if(i>=j)
 				{
-		System.out.println(*);
-	}
+		                         System.out.print("*");
+	                                            }
+                                                             else
+                                                                {
+                                                             System.out.print(" ");
+                                                               }
+                                                          }
+                                                             System.out.println();
+                                                
+                                                }}
+                            
 }
